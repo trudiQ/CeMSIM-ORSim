97cd24588e5be245805a845cde597996268bc673
Adam <adam.ryason@live.com>
2020-10-29 12:35:56 -0400

    Add Lung and Needle Behaviors
    - Added Lung (LoFi) prefab with behavior for trigger from Tool tags
    - Added collider to Catheters for interaction with the Lung

---
+++ Assets/NeedleBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class NeedleBehavior : MonoBehaviour
+{
+    XRGrabInteractable _xrInteractable;
+    Rigidbody _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _xrInteractable = this.GetComponent<XRGrabInteractable>();
+        _rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NeedleInserted(bool isInserted)
+    {
+        if(isInserted)
+        {
+            _xrInteractable.gravityOnDetach = false;
+            _rb.constraints = RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        }
+        else
+        {
+            _xrInteractable.gravityOnDetach = true;
+            _rb.constraints = RigidbodyConstraints.None;
+        }
+    }
+}
---
+++ Assets/VORTEX/Scripts/LungBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LungBehavior : MonoBehaviour
+{
+    public Material originalMaterial;
+    public Material enterMaterial;
+
+    private MeshRenderer meshRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = this.GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Tool")
+        {
+            //var tool = other.GetComponent<UnityEngine.XR.Interaction.Toolkit.XRGrabInteractable>();
+            meshRenderer.material = enterMaterial;
+            other.GetComponent<NeedleBehavior>().NeedleInserted(true);
+            //tool.gravityOnDetach = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Tool")
+        {
+            //var tool = other.GetComponent<UnityEngine.XR.Interaction.Toolkit.XRGrabInteractable>();
+            meshRenderer.material = originalMaterial;
+            other.GetComponent<NeedleBehavior>().NeedleInserted(false);
+            //tool.gravityOnDetach = true;
+        }
+    }
+}
