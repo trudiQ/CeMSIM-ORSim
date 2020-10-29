c0fbaa977a6f7897ad947fef68468e559ae16723
Mac Harris <bharris8@cub.uca.edu>
2020-10-29 11:22:40 -0500

    added event support for pulse engine

---
+++ Assets/VORTEX/Scripts/Pulse/PulseEventManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Pulse.CDM
+{
+    public class PulseEventManager : MonoBehaviour
+    {
+        public PulseActionManager actionManager;
+
+        public delegate void TriggerAction();
+        public static event TriggerAction triggerAction;
+
+        public void TriggerPulseAction(PulseAction action)
+        {
+            actionManager.action = action;
+
+            triggerAction?.Invoke();
+        }
+    }
+}
