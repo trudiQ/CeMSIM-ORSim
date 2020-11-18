519f5c5bea56e9698169280885aa92e7af78a171
Adam <adam.ryason@live.com>
2020-11-18 14:04:08 -0500

    Modified Tool scipts
    - Removed includes that may be causing bugs in future releases of Unity.

--- Assets/VCOST/Scripts/Tools/SelectTool.cs
+++ Assets/VCOST/Scripts/Tools/SelectTool.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.CodeDom;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Data;
-using System.Runtime.InteropServices;
-using System.Security.Policy;
 using UnityEditor;
 using UnityEngine;
 
