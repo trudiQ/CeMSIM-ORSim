f5944d787b4c3a690491f92692eaaf686fc5624b
Doga Demirel <dxdemirel@gmail.com>
2020-12-02 10:44:33 -0500

    Added missing files

---
+++ Assets/VCOST/Scripts/Tools/SelectTool.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using UnityEditor;
+using UnityEngine;
+
+public class SelectTool : MonoBehaviour
+{
+    public bool Enable;
+
+    void Start()
+    {
+        print("Select a tool; Main Camera enabled.");
+    }
+    void FixedUpdate()
+    {
+        RunChildren(); //checks if linear stapler component is selected in each frame
+    }
+
+    public void ClickAndCount()
+    {
+        //Sets enable to true and prints name of selected tool when mouse left-clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 1000.0f))
+            {
+                if (hit.transform && hit.transform.gameObject.GetComponent<Tool>())
+                {
+                    hit.transform.gameObject.GetComponent<Tool>().Enable = true;
+                    print(hit.transform.gameObject.name + " selected; Main Camera disabled.");
+                    Enable = true;
+                }
+            }
+        }
+        //Sets enable to false, prints name of deselected tool when mouse right-clicked, gives camera option
+        else if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 1000.0f))
+            {
+                if (hit.transform && hit.transform.gameObject.GetComponent<Tool>())
+                {
+                    hit.transform.gameObject.GetComponent<Tool>().Enable = false;
+                    print(hit.transform.gameObject.name + " deselected; Press 'Esc' to enable Main Camera only.");
+                    Enable = false;
+                }
+            }
+        }
+        //If escape button is pressed, all tool movement is disabled and camera is enabled
+        else if (Input.GetKey(KeyCode.Escape))
+        {
+            Enable = false;
+            print("No selections; Main Camera enabled.");
+            foreach (Transform child in this.transform)
+            {
+                child.GetComponent<Tool>().Enable = false;
+            }
+        }
+    }
+
+    public void RunChildren() //Enables consecutive selection of linear stapler components
+    { 
+        Tools[] children = new Tools[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            ClickAndCount();
+        }
+    }
+}
---
+++ Assets/VCOST/Scripts/Tools/Tool.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityScript.Steps;
+
+public class Tool : MonoBehaviour //inherits SelectTool class
+{
+    public float speed;
+    public float AngularSpeed;
+    public bool Enable;
+
+    private Vector3 movement;
+    private float x, y, z;
+    private float angle = 0;
+    private Rigidbody m_tool;
+
+    void Start()
+    {
+        m_tool = this.GetComponent<Rigidbody>(); //references rigidbody component of selected tool
+    }
+
+    void Update()
+    {
+        Enabler(); //checks for selected tools in each frame
+    }
+
+    void FixedUpdate()
+    {
+        movePlayer(movement); //calls velocity function
+    }
+
+    void movePlayer(Vector3 direction)
+    {
+        m_tool.velocity = direction * speed; //defines velocity function
+    }
+
+    public void TranslationalInput()
+    {
+        //Sets WASD as the directional keys for moving an object forward, back, left, and right
+        x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        angle = AngularSpeed * Time.deltaTime;
+
+        //Sets Q & E as the directional keys for moving an object up and down
+        if (Input.GetKey(KeyCode.Q))
+        {
+            y = speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            y = -speed * Time.deltaTime;
+        }
+        else
+        {
+            y = 0;
+        }
+
+        //Directional key variable updates the parameters of an object in 3-dimensions
+        movement = new Vector3(x, y, z);
+    }
+
+    public void RotationalInput()
+    {
+        //y-axis rotation
+        if (Input.GetKey(KeyCode.L))
+        {
+            transform.Rotate(Vector3.down * angle, Space.Self);
+        }
+        else if (Input.GetKey(KeyCode.J))
+        {
+            transform.Rotate(Vector3.up * angle, Space.Self);
+        }
+
+        //z-axis rotation
+        if (Input.GetKey(KeyCode.U))
+        {
+            transform.Rotate(Vector3.back * angle, Space.Self);
+        }
+        else if (Input.GetKey(KeyCode.O))
+        {
+            transform.Rotate(Vector3.forward * angle, Space.Self);
+        }
+
+        //x-axis rotation
+        if (Input.GetKey(KeyCode.K))
+        {
+            transform.Rotate(Vector3.right * angle, Space.Self);
+        }
+        else if (Input.GetKey(KeyCode.I))
+        {
+            transform.Rotate(Vector3.left * angle, Space.Self);
+        }
+    }
+
+    public void Enabler() //Allows translation and rotation of selected tool
+    {
+        if (Enable == true)
+        {
+            TranslationalInput();
+            RotationalInput();
+        }
+        else
+        {
+            
+        }
+    }
+}
