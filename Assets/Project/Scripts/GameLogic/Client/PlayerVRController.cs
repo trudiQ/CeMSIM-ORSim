473d33c98dfd8127fff34e3ff0a271e4a097ba79
JRBarker <jacobryanbarker@gmail.com>
2021-02-05 08:16:15 -0500

    Updated with assets from VCOST branch

---
+++ Assets/Project/Scripts/GameLogic/Client/PlayerVRController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using CEMSIM.Network;
+
+namespace CEMSIM
+{
+    namespace GameLogic
+    {
+        public class PlayerVRController : MonoBehaviour
+        {
+            public Transform VRCamera;
+
+            private void FixedUpdate()
+            {
+                ClientSend.PlayerVRMovement();
+            }
+        }
+    }
+}
