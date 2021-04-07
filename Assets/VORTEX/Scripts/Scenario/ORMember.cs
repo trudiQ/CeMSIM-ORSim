f55828d95be486e5752f123a834ffd4ccbfcda87
Adam <adam.ryason@live.com>
2021-04-07 10:32:18 -0400

    Added ORMember class, organized scripts
    - Added ORMember class for keeping tracking of Member-specific information and events.
    - Added Event for equipping a tool to a Member when selecting interactables.

---
+++ Assets/VORTEX/Scripts/Scenario/ORMember.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace CEMSIM
+{
+    public class ORMember : MonoBehaviour
+    {
+        [Header("Tool Ownership")]
+        public GameObject leftTool;
+        public GameObject rightTool;
+
+        [Header("References")]
+        public XRManager xRManager; //TO DO: Make XRManager a singleton (AR 4/7/21)
+    
+        /// <summary>
+        /// Equips a given game object in the OR Member's hand based on the selected interactable.
+        /// </summary>
+        /// <param name="right"></param>
+        /// <param name="tool"></param>
+        public void EquipTool(bool right)
+        {          
+            if (right)
+            {
+                //TO DO: Update to work with SteamVR and XR Manager (AR 4/7/21)
+                XRDirectInteractor controller = xRManager.RightHandController.GetComponent<XRDirectInteractor>();
+                Debug.Log(controller.selectTarget.gameObject);
+                rightTool = controller.selectTarget.gameObject;
+            }
+            else
+            {
+                //TO DO: Update to work with SteamVR and XR Manager (AR 4/7/21)
+                XRDirectInteractor controller = xRManager.LeftHandController.GetComponent<XRDirectInteractor>();
+                leftTool = controller.selectTarget.gameObject;
+            }
+        }
+
+        /// <summary>
+        /// Unequips the tool being held by the OR Member in a given hand.
+        /// </summary>
+        /// <param name="right">True if targetting the right hand.</param>
+        public void UnequipTool(bool right)
+        {
+            if(right)
+            {
+                rightTool = null;
+            }
+            else
+            {
+                leftTool = null;
+            }
+        }
+    }
+}
