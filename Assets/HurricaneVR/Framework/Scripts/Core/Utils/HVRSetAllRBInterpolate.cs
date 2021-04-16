88e6164b9f9b61b58887bc08c6e4d745e4232f4d
Adam <ary1111@users.noreply.github.com>
2021-04-16 19:43:32 -0400

    Added HurricanVR, FinalIK, and PuppetMaster packages

---
+++ Assets/HurricaneVR/Framework/Scripts/Core/Utils/HVRSetAllRBInterpolate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace HurricaneVR.Framework.Utils
+{
+    public class HVRSetAllRBInterpolate : MonoBehaviour
+    {
+        public void Awake()
+        {
+            foreach (var rb in FindObjectsOfType<Rigidbody>())
+            {
+                rb.interpolation = RigidbodyInterpolation.Interpolate;
+            }
+        }
+    }
+}
