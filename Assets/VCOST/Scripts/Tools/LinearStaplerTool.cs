6baba077df0433b9e962c492043f878b7b23b8cd
snowtelevision <pguan100@gmail.com>
2021-04-16 16:34:29 -0400

    work on LS prefab and script so that existing code work with the new LS model

--- Assets/VCOST/Scripts/Tools/LinearStaplerTool.cs
+++ Assets/VCOST/Scripts/Tools/LinearStaplerTool.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class LinearStaplerTool : Tool //inherits Tool class
 {
     public GameObject FiringHandle;
+    public Transform firingHandleStartPosition;
+    public Transform firingHandleEndPosition;
+    public bool handlePushed;
     public GameObject LockingLever;
+    public bool leverLocked;
 
     void Update() //Checks status of knob, lever, and linear stapler in every frame
     {
@@ -16,41 +20,43 @@ public class LinearStaplerTool : Tool //inherits Tool class
 
     void CheckFiringKnob()
     {
-        //Knob translates to final position if T is pressed and linear stapler is enabled
-        Vector3 Knob = FiringHandle.transform.localPosition;
-        if ((Input.GetKeyUp(KeyCode.T)) && (Knob.x > 0) && (Enable == true))
+        if (Enable && Input.GetKeyUp(KeyCode.T))
         {
-            Knob.x = -42;
-            FiringHandle.transform.localPosition = Knob;
-            print("Firing Handle is engaged.");
-        }
-        //Knob translates to initial position if T is pressed again and linear stapler is enabled
-        else if (Input.GetKeyUp(KeyCode.T) && (Enable == true))
-        {
-            Knob.x = 21;
-            FiringHandle.transform.localPosition = Knob;
-            print("Firing Handle is not engaged.");
+            // Handle translates to final position if T is pressed and linear stapler is enabled
+            if (!handlePushed)
+            {
+                FiringHandle.transform.localPosition = firingHandleEndPosition.localPosition;
+                print("Firing Handle is engaged.");
+            }
+            // Handle translates to initial position if T is pressed again and linear stapler is enabled
+            else
+            {
+                FiringHandle.transform.localPosition = firingHandleStartPosition.localPosition;
+                print("Firing Handle is not engaged.");
+            }
 
+            handlePushed = !handlePushed;
         }
     }
 
     void CheckLockingLever()
     {
-        //Locking lever closes if Y is pressed and linear stapler is enabled
-        Quaternion Hinge = LockingLever.transform.localRotation;
-        if ((Input.GetKeyUp(KeyCode.Y)) && (Hinge.z < 0.0) && (Enable == true))
+        if (Enable && Input.GetKeyUp(KeyCode.Y))
         {
-            Hinge.z = 0.0f;
-            LockingLever.transform.localRotation = Hinge;
-            print("Locking Lever is closed.");
-        }
-        //Locking lever opens if Y is pressed again and linear stapler is enabled
-        else if (Input.GetKeyUp(KeyCode.Y) && (Enable == true))
-        {
-            Hinge.z = -0.2f;
-            LockingLever.transform.localRotation = Hinge;
-            print("Locking Lever is open.");
+            //Locking lever closes if Y is pressed and linear stapler is enabled
+            if (!leverLocked)
+            {
+                LockingLever.transform.localEulerAngles = Vector3.zero;
+                print("Locking Lever is closed.");
+            }
+            //Locking lever opens if Y is pressed again and linear stapler is enabled
+            else
+            {
+                LockingLever.transform.localEulerAngles = new Vector3(0, 15, 0);
+                print("Locking Lever is open.");
+            }
 
+            leverLocked = !leverLocked;
         }
     }
 }
