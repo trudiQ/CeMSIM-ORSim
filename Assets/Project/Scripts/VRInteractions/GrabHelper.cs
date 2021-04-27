8a658ed0c2b53908aefff82c6c6e4c28d773497d
mharr42 <bharris8@cub.uca.edu>
2021-04-27 18:11:42 -0500

    added vr interactions and minor optimization
    -added decompression needle model, grab/stab interactions
    -added interaction with OR equipment
    -disabled sky and fog volume

---
+++ Assets/Project/Scripts/VRInteractions/GrabHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabHelper : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LockRB()
+    {
+        
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionY;
+    }
+
+    public void UnlockRB()
+    {
+        rb.constraints = RigidbodyConstraints.None;
+        
+    }
+}
