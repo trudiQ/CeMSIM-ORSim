192be5cbb575bfc3763a138114940b3cd9233ab8
mharr42 <bharris8@cub.uca.edu>
2021-04-28 23:39:42 -0500

    added more HVR interactions
    -improved interactions on equipment
    -added rudimentary patient interactions
    -fixed interactable buttons
    -added drawer interactions but they're kind of broken still
    -added rudimentary cloth

--- Assets/Project/Scripts/VRInteractions/GrabHelper.cs
+++ Assets/Project/Scripts/VRInteractions/GrabHelper.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// using HurricaneVR.Framework.Components;
 
 public class GrabHelper : MonoBehaviour
 {
     public Rigidbody rb;
+    public Collider[] colliders;
+
+    public GameObject otherHVRGrabbableGO;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,52 @@ public class GrabHelper : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void IgnorePlayerCollision(bool doit)
+    {
+        
+        if(doit)
+        {
+            foreach(Collider col in colliders)
+            {
+                col.gameObject.layer = 22;
+            }
+        }
+        else
+        {
+            foreach(Collider col in colliders)
+            {
+                col.gameObject.layer = 12;
+            }
+        }
+        
+        
+    }
+
+    // public void EnableOther()
+    // {
+    //     otherHVRGrabbable.GetComponent<HVRGrabbable>().enabled = true;
+    // }
+    // public void DisableOther()
+    // {
+    //     otherHVRGrabbable.GetComponent<HVRGrabbable>().enabled = false;
+    // }
+
+    public void SetOtherKinematic(bool state)
+    {
+        otherHVRGrabbableGO.GetComponent<Rigidbody>().isKinematic = state;
+        otherHVRGrabbableGO.GetComponent<Rigidbody>().useGravity = !state;
+    }
+
+    public void SetKinematic(bool state)
+    {
+        rb.isKinematic = state;
+    }
+
+    public void SetWeight(int weight)
+    {
+        rb.mass = weight;
     }
 
     public void LockRB()
---
+++ Assets/Project/Scripts/VRInteractions/IgnoreColliders.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreColliders : MonoBehaviour
+{
+    public Collider[] collidersToIgnore;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach(Collider col in collidersToIgnore)
+        {
+            if(col)
+            {
+                Physics.IgnoreCollision(col, this.GetComponent<Collider>(), true);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
