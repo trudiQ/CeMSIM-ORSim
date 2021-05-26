9427441048913e30d29d25e47c00e83bfe32df34
mac-desktop <trainsientbiscuit@gmail.com>
2021-05-26 00:04:11 -0500

    added basic HVR equipment interactions
    -adjusted origin points for several objects
    -added basic grabbing and movement for reduced equipment
    -created new prefabs for interactable equipment
    -expanded grab helper script

--- Assets/Project/Scripts/VRInteractions/GrabHelper.cs
+++ Assets/Project/Scripts/VRInteractions/GrabHelper.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using HurricaneVR.Framework.Core.Player;
 // using HurricaneVR.Framework.Components;
 
 public class GrabHelper : MonoBehaviour
 {
     public Rigidbody rb;
     public Collider[] colliders;
+    public HVRPlayerController player;
 
     public GameObject otherHVRGrabbableGO;
+    private float initialPlayerAcceleration;
+    private float intialPlayerMovementSpeed;
+    private float intitalPlayerTurnSpeed;
+    private float initialPlayerDeceleration;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPlayerAcceleration = player.Acceleration;
+        intialPlayerMovementSpeed = player.MoveSpeed;
+        intitalPlayerTurnSpeed = player.SmoothTurnSpeed;
+        initialPlayerDeceleration = player.Deacceleration;
     }
 
     // Update is called once per frame
@@ -20,6 +30,45 @@ public class GrabHelper : MonoBehaviour
     {
         
     }
+
+    public void SetPlayerAcceleration(float a)
+    {
+        player.Acceleration = a;
+    }
+
+    public void SetPlayerAccelerationToInitialValue()
+    {
+        SetPlayerAcceleration(initialPlayerAcceleration);
+    }
+
+    public void SetPlayerSpeed(float s)
+    {
+        player.MoveSpeed = s;
+    }
+    public void SetPlayerSpeedToInitialVallue()
+    {
+        SetPlayerSpeed(intialPlayerMovementSpeed);
+    }
+
+    public void SetPlayerTurnSpeed(float s)
+    {
+        player.SmoothTurnSpeed = s;
+    }
+    public void SetPlayerTurnSpeedToInitialValue()
+    {
+        SetPlayerTurnSpeed(intitalPlayerTurnSpeed);
+    }
+
+    public void SetPlayerDeceleration(float d)
+    {
+        player.Deacceleration = d;
+    }
+    public void SetPlayerDecelerationToInitialValue()
+    {
+        player.Deacceleration = initialPlayerDeceleration;
+    }
+
+
 
     public void IgnorePlayerCollision(bool doit)
     {
