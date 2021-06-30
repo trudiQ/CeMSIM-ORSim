f589b35e15ab6cffc9f651053cecff04b168c444
Methunas <b1756chris@aol.com>
2021-06-30 16:49:55 -0400

    Added NetRig and removed old model file

--- Assets/Project/Scripts/Avatars/AvatarComponents.cs
+++ Assets/Project/Scripts/Avatars/AvatarComponents.cs
@@ -5,8 +5,14 @@ using HurricaneVR.Framework.Core;
 
 public class AvatarComponents : MonoBehaviour
 {
+    [Header("HVRGlobal Components")]
     public HVRPlayerController playerController;
     public Transform rigCamera;
+
+    [Header("Networked Components")]
+    public Transform leftControllerPosition;
+    public Transform rightControllerPosition;
+    public Transform hmdPosition;
 
     public void SetManagerComponents(HVRManager manager)
     {
