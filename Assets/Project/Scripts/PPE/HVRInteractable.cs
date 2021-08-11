83b89e1333f60aa266c7763211a1672a9f651442
Methunas <b1756chris@aol.com>
2021-08-10 23:46:05 -0400

    Added an interactable script
    Makes objects able to be interacted with but not grabbed

---
+++ Assets/Project/Scripts/PPE/HVRInteractable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HurricaneVR.Framework.Core;
+using HurricaneVR.Framework.Core.Grabbers;
+
+public class HVRInteractable : HVRGrabbable
+{
+    [Header("Interactable Options")]
+    [Tooltip("A toggle that makes this object either grabbable or interactable.")]
+    public bool grabbable = false;
+
+    protected override void OnGrabbed(HVRGrabberBase grabber)
+    {
+        if (grabbable)
+            base.OnGrabbed(grabber);
+        else if (grabber.IsHandGrabber)
+        {
+            HandGrabbed.Invoke(grabber as HVRHandGrabber, this);
+            ForceRelease();
+        }
+    }
+}
