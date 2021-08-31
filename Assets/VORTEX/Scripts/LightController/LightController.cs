c18bc3bcfb6c1b3ddd31fb7e8cb06a1f1b900a90
mac-desktop <trainsientbiscuit@gmail.com>
2021-08-31 13:46:31 -0500

    visual improvements
    -updating lighting (modified active lights, turned off static lighting)
    -added indirect lighting controller
    -set up light switch to switch between overhead lights and surgical lamps
    -updated materials (lightening and applying new textures)
    -added textures (anesthesia machine, floor, walls)
    -updated colliders drape setup
    -tweaked obi cloth settings
    -corrected some missing reference errors on alarm mute button

---
+++ Assets/VORTEX/Scripts/LightController/LightController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+public class LightController : MonoBehaviour
+{
+    public Material lightOnMaterial;
+    public Material lightOffMaterial;
+    public Light overheadLight;
+    public MeshRenderer overheadLightMesh;
+    public Light[] spotLights;
+    public MeshRenderer[] articulatingLights;
+    public Volume lightingVolume;
+    private VolumeProfile profile;
+    private IndirectLightingController indirectLightingController;
+    private bool lightsToggled = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeIndirectLightController();
+    }
+
+
+    private void InitializeIndirectLightController()
+    {
+        IndirectLightingController temp;
+        if(lightingVolume.profile.TryGet<IndirectLightingController>(out temp))
+        {
+            indirectLightingController = temp;
+        }
+
+        // indirectLightingController.active = false;
+        indirectLightingController.indirectDiffuseIntensity.value = 1;
+        
+    }
+
+    public void ToggleLights()
+    {
+        if(!lightsToggled)
+        {
+            TurnOverheadLightsOff();
+            lightsToggled = true;
+        }
+        else
+        {
+            TurnOverheadLightsOn();
+            lightsToggled = false;
+        }
+    }
+
+    public void ToggleIndirectLighting(bool state)
+    {
+        indirectLightingController.active = state;
+    }
+
+    public void TurnOverheadLightsOff()
+    {
+        overheadLightMesh.material = lightOffMaterial;
+        overheadLight.enabled = false;
+        foreach(var light in spotLights)
+        {
+            light.enabled = true;
+        }
+        foreach(var light in articulatingLights)
+        {
+            light.material = lightOnMaterial;
+        }
+        // ToggleIndirectLighting(true);
+        indirectLightingController.indirectDiffuseIntensity.value = .3f;
+
+    }
+
+    public void TurnOverheadLightsOn()
+    {
+        
+        overheadLightMesh.material = lightOnMaterial;
+        overheadLight.enabled = true;
+        foreach(var light in spotLights)
+        {
+            light.enabled = false;
+        }
+        foreach(var light in articulatingLights)
+        {
+            light.material = lightOffMaterial;
+        }
+        // ToggleIndirectLighting(false);
+        indirectLightingController.indirectDiffuseIntensity.value = 1f;
+
+    }
+}
