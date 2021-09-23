d3e1e532aefd4bce391961bfa81a6deca3bc6d78
Yasar <ykakdas@gmail.com>
2021-09-22 21:09:54 -0500

    Created teleportation pad prefab with teleporter script. Added teleportable tag for teleporting only avatar.

---
+++ Assets/VORTEX_COVID/Scripts/Teleporter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public Transform destination;
+    public bool disableAfterArrival;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Teleportable")
+        {
+            other.transform.position = destination.position;
+            if (disableAfterArrival)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+}
