4a211401f12538caff344384242ef5f4d1c7005b
Methunas <b1756chris@aol.com>
2021-09-30 14:39:38 -0400

    Additional script to control glove visibility

---
+++ Assets/VORTEX_COVID/Scripts/Cloth/MultiGloveToggle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiGloveToggle : MonoBehaviour
+{
+    public GameObject gloveObjectWithSkinnedMesh;
+    public GameObject handObjectWithSkinnedMesh;
+    public int gloveCount;
+
+    private int currentGloveEquippedCount = 0;
+
+    public void GloveEquipped()
+    {
+        if (++currentGloveEquippedCount >= 1)
+        {
+            gloveObjectWithSkinnedMesh.SetActive(true);
+            handObjectWithSkinnedMesh.SetActive(false);
+        }
+    }
+
+    public void GloveUnequipped()
+    {
+        if (--currentGloveEquippedCount == 0)
+        {
+            gloveObjectWithSkinnedMesh.SetActive(false);
+            handObjectWithSkinnedMesh.SetActive(true);
+        }
+    }
+}
