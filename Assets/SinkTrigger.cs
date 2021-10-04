7a11270abb5e4c7ad4d222effe1ea3bca1f667f7
Yasar <ykakdas@gmail.com>
2021-10-04 17:44:28 -0500

    Made sink water enabled by proximity.

---
+++ Assets/SinkTrigger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class SinkTrigger : MonoBehaviour
+{
+    public UnityEvent triggerEnter;
+    public UnityEvent triggerExit;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            triggerEnter.Invoke();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            triggerExit.Invoke();
+        }
+    }
+}
