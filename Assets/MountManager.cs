4b98af5958ef35972bedb49d6f65df6cfb9ebc0a
Yasar <ykakdas@gmail.com>
2021-11-22 19:38:47 -0600

    Added template of assembling and disassembling blades.

---
+++ Assets/MountManager.cs
@@ -0,0 +1,42 @@
+﻿using HurricaneVR.Framework.Core;
+using HurricaneVR.Framework.Core.Grabbers;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MountManager : MonoBehaviour {
+
+	[HideInInspector]
+	public bool isAttached;
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(isAttached) return;
+		Mountable mountable = other.gameObject.GetComponent<Mountable>();
+		Debug.Log(mountable);
+		if(mountable) {
+			Transform otherTransform = other.gameObject.transform;
+			otherTransform.parent = transform;
+			otherTransform.localPosition = mountable.desiredPosition;
+			otherTransform.localRotation = Quaternion.identity;
+			otherTransform.SetParent(null, true);
+
+			GameObject otherGameObject = other.gameObject;
+			FixedJoint joint = otherGameObject.AddComponent<FixedJoint>();
+			joint.breakForce = 150f;
+			joint.breakTorque = 150f;
+			joint.connectedBody = GetComponent<Rigidbody>();
+
+			mountable.grabbable.ForceRelease();
+			isAttached = true;
+		}
+	}
+
+}
---
+++ Assets/Mountable.cs
@@ -0,0 +1,23 @@
+﻿using HurricaneVR.Framework.Core;
+using UnityEngine;
+
+public class Mountable : MonoBehaviour {
+	public Vector3 desiredPosition;
+	public HVRGrabbable grabbable;
+	public MountManager manager;
+
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		
+	}
+
+	void OnJointBreak(float breakForce) {
+		manager.isAttached = false;
+	}
+
+}
