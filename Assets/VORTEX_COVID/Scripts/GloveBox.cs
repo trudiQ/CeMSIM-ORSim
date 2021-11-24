8758edb8787576d06ff8134211fc92e2d82dbf69
Methunas <b1756chris@aol.com>
2021-11-24 13:12:10 -0500

    Added glove box and PPE spawner

---
+++ Assets/VORTEX_COVID/Scripts/GloveBox.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HurricaneVR.Framework.Core.Grabbers;
+using HurricaneVR.Framework.Shared;
+
+[RequireComponent(typeof(HVRInteractable))]
+public class GloveBox : MonoBehaviour
+{
+    public GameObject leftGlovePrefab;
+    public GameObject rightGlovePrefab;
+
+    private HVRInteractable interactable;
+
+    private void Start()
+    {
+        interactable = GetComponent<HVRInteractable>();
+
+        interactable.Interacted.AddListener(OnInteracted);
+    }
+
+    private void OnInteracted(HVRHandGrabber grabber, HVRInteractable interactable)
+    {
+        if (grabber.HandSide == HVRHandSide.Left)
+        {
+            GameObject glove = Instantiate(rightGlovePrefab, grabber.transform.position, grabber.transform.rotation);
+            HVRInteractable gloveInteractable = glove.GetComponent<HVRInteractable>();
+            grabber.TryGrab(gloveInteractable, true);
+        }
+        else
+        {
+            GameObject glove = Instantiate(leftGlovePrefab, grabber.transform.position, grabber.transform.rotation);
+            HVRInteractable gloveInteractable = glove.GetComponent<HVRInteractable>();
+            grabber.TryGrab(gloveInteractable, true);
+        }
+    }
+}
---
+++ Assets/VORTEX_COVID/Scripts/PPESpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PPESpawner : MonoBehaviour
+{
+    public GameObject ppePrefab;
+    public float respawnThresholdDistance = 0.5f;
+
+    private GameObject currentlyTrackedObject;
+
+    private void Start()
+    {
+        Instantiate(ppePrefab, transform.position, transform.rotation);
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(currentlyTrackedObject.transform.position, transform.position) > respawnThresholdDistance)
+            Instantiate(ppePrefab, transform.position, transform.rotation);
+    }
+}
