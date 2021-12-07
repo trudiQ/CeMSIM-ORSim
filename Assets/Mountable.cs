683fc4101746998421f71c722f3921b53fb26e87
Yasar <ykakdas@gmail.com>
2021-12-06 23:11:33 -0600

    Constant break force converted into variable.

--- Assets/Mountable.cs
+++ Assets/Mountable.cs
@@ -5,8 +5,11 @@ public class Mountable : MonoBehaviour {
 	public Vector3 desiredPosition;
 	public HVRGrabbable grabbable;
 	public MountManager manager;
+	public float breakForce = 150f;
+
 	[HideInInspector]
 	public bool mounted;
+
 	private FixedJoint joint;
 
 	// Start is called before the first frame update
@@ -16,15 +19,15 @@ public class Mountable : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
-		if(mounted) {
+		if (mounted) {
 			joint = GetComponent<FixedJoint>();
 			mounted = false;
 		}
-		if(joint) {
+		if (joint) {
 			Vector3 projection = Vector3.Project(joint.currentForce, transform.right);
-			if(projection.magnitude > 150) {
+			if (projection.magnitude > breakForce) {
 				Destroy(joint);
-				OnJointBreak(150f);
+				OnJointBreak(projection.magnitude);
 			}
 		}
 
