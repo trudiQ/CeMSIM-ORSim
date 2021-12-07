72ded116222811faafaf060c71ec1d8fcbd4b2d6
Yasar <ykakdas@gmail.com>
2021-12-06 23:07:17 -0600

    Added endotracheal tube models and interactions.

---
+++ Assets/StyletInteraction.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class StyletInteraction : MonoBehaviour {
+	[HideInInspector]
+	public bool isDetached = false;
+
+	public GameObject tube;
+	public Transform anchor;
+	public Collider collider;
+	public bool useMotor = false;
+
+	private Rigidbody tubeRigidbody;
+	private Rigidbody styletRigidbody;
+	// Start is called before the first frame update
+	void Start() {
+		tubeRigidbody = tube.GetComponent<Rigidbody>();
+		styletRigidbody = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	public void OnStyletGrabbed() {
+		if (!isDetached) {
+			tubeRigidbody.constraints = RigidbodyConstraints.FreezeAll;
+			HingeJoint joint = gameObject.AddComponent<HingeJoint>();
+			joint.anchor = anchor.transform.localPosition;
+			joint.axis = Vector3.forward;
+			if (useMotor) {
+				var motor = joint.motor;
+				motor.force = 50;
+				motor.targetVelocity = -80;
+				motor.freeSpin = false;
+				joint.motor = motor;
+				joint.useMotor = true;
+			}
+		}
+	}
+
+	public void OnStyletReleased() {
+		if (!isDetached) {
+			tube.GetComponent<TubeInteraction>().Regrab();
+			styletRigidbody.isKinematic = true;
+			RemoveJoint();
+		} else {
+			collider.isTrigger = false;
+			styletRigidbody.isKinematic = false;
+			transform.SetParent(null, true);
+		}
+	}
+
+	public void RemoveJoint() {
+		Destroy(GetComponent<HingeJoint>());
+		tubeRigidbody.constraints = RigidbodyConstraints.None;
+	}
+}
---
+++ Assets/TubeInteraction.cs
@@ -0,0 +1,44 @@
+﻿using HurricaneVR.Framework.Core;
+using HurricaneVR.Framework.Core.Grabbers;
+using UnityEngine;
+
+public class TubeInteraction : MonoBehaviour {
+	public Collider tubeIgnoreCollider;
+	public Collider styletIgnoreCollider;
+
+	private HVRGrabbable grabbable;
+	private HVRHandGrabber grabber;
+	// Start is called before the first frame update
+	void Start() {
+		grabbable = GetComponent<HVRGrabbable>();
+		grabbable.HandGrabbed.AddListener(OnGrabbed);
+
+		Physics.IgnoreCollision(tubeIgnoreCollider, styletIgnoreCollider);
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		StyletInteraction styletInteraction = other.gameObject.GetComponent<StyletInteraction>();
+
+		if (styletInteraction == null || styletInteraction.isDetached) return;
+		Debug.Log(other);
+
+		styletInteraction.RemoveJoint();
+		styletInteraction.isDetached = true;
+		Regrab();
+	}
+
+	public void Regrab() {
+		grabber.ForceRelease();
+		grabber.TryGrab(grabbable, true);
+	}
+
+	private void OnGrabbed(HVRHandGrabber grabber, HVRGrabbable grabbable) {
+		this.grabber = grabber;
+	}
+
+}
