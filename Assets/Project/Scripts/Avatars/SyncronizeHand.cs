e7e94cae9d00cd3a1174c288e7d5626ff43eba18
mac-desktop <60515631+mharr42@users.noreply.github.com>
2021-12-10 09:30:47 -0600

    Improved Dynamic Grabbing
    - improved dynamic grabbing using detached hands with colliders
    - added detached hand models
    - added script to synchronize avatar hands with invisible detached hands
    - renamed some avatar related files to match new naming convention
    - modified HVRSettings to use detached hands for posing
    - updated AdultAfricanMale VRIKRig and swapping details

---
+++ Assets/Project/Scripts/Avatars/SyncronizeHand.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HurricaneVR.Framework.Shared.HandPoser;
+
+public class SyncronizeHand : MonoBehaviour
+{
+    public List<Transform> colliderTransforms;
+    public List<Transform> visualTransforms;
+
+    // Update is called once per frame
+    void Update()
+    {
+        int i = 0;
+        foreach(Transform t in colliderTransforms)
+        {
+            visualTransforms[i].rotation = t.rotation;
+            i++;
+        }
+
+    }
+}
