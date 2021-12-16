2c8b3e025fb5260077012b14f74b33a9091d83ab
Methunas <b1756chris@aol.com>
2021-12-16 14:47:30 -0500

    Added video player

---
+++ Assets/VORTEX_COVID/Scripts/Utility/VideoPlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class VideoPlayerController : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+
+    private void Awake()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    public void PlayClip()
+    {
+        videoPlayer.Play();
+    }
+}
