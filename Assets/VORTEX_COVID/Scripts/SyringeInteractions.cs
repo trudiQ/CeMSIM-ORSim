e970363421bf68c8b54bd61bfcfb0a9992d98d2d
Yasar <ykakdas@gmail.com>
2021-12-18 01:50:58 -0600

    Added syringe-plunger interactions, hvr global inputs for button click events.

---
+++ Assets/VORTEX_COVID/Scripts/SyringeInteractions.cs
@@ -0,0 +1,34 @@
+﻿using HurricaneVR.Framework.Shared.HandPoser;
+using UnityEngine;
+
+public class SyringeInteractions : MonoBehaviour {
+
+	public GameObject plunger;
+	public Vector3 plungerStartPos;
+	public Vector3 plungerEndPos;
+	public float speed = 16;
+	public HVRHandPoser poser;
+
+	public bool isGrabbed { get; set; } = false;
+	public bool isPrimaryButtonPressed { get; set; } = false;
+
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (isGrabbed) {
+			if (isPrimaryButtonPressed) {
+				poser.PrimaryPose.Type = BlendType.BooleanParameter;
+				plunger.transform.localPosition = Vector3.Lerp(plunger.transform.localPosition, plungerEndPos, Time.deltaTime * speed);
+			} else {
+				plunger.transform.localPosition = Vector3.Lerp(plunger.transform.localPosition, plungerStartPos, Time.deltaTime * speed);
+			}
+		} else {
+			poser.PrimaryPose.Type = BlendType.Immediate;
+		}
+	}
+
+}
