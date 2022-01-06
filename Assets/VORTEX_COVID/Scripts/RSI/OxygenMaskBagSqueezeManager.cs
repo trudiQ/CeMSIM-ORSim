e4b95a631218a756ac8e5542665c18e0108d7f08
Yasar <ykakdas@gmail.com>
2022-01-05 20:00:52 -0600

    Added double-handed grabbing for ventilation bag.

--- Assets/VORTEX_COVID/Scripts/RSI/OxygenMaskBagSqueezeManager.cs
+++ Assets/VORTEX_COVID/Scripts/RSI/OxygenMaskBagSqueezeManager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 public class OxygenMaskBagSqueezeManager : MonoBehaviour {
 
 	public SkinnedMeshRenderer meshRenderer;
-	public HVRHandPoser poser;
-	public bool isGrabbed { get; set; } = false;
-	public bool isPrimaryButtonPressed { get; set; } = false;
+	public HVRHandPoser leftHandPoser;
+	public HVRHandPoser rightHandPoser;
+
+	public bool isLeftPrimaryButtonPressed { get; set; } = false;
+	public bool isRightPrimaryButtonPressed { get; set; } = false;
 	public float speed = 16;
+
+	private int grabCount = 0;
 
 	// Start is called before the first frame update
 	void Start() {
@@ -16,17 +20,30 @@ public class OxygenMaskBagSqueezeManager : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
-		if (isGrabbed) {
-			if (isPrimaryButtonPressed) {
+		if (grabCount != 0) {
+			if (isLeftPrimaryButtonPressed) {
 				float value = Mathf.Clamp(meshRenderer.GetBlendShapeWeight(0) + Time.deltaTime * speed * 100, 0, 100);
 				meshRenderer.SetBlendShapeWeight(0, value);
-				poser.PrimaryPose.Type = BlendType.BooleanParameter;
-			} else {
+				leftHandPoser.PrimaryPose.Type = BlendType.BooleanParameter;
+			}
+			if (isRightPrimaryButtonPressed) {
+				float value = Mathf.Clamp(meshRenderer.GetBlendShapeWeight(0) + Time.deltaTime * speed * 100, 0, 100);
+				meshRenderer.SetBlendShapeWeight(0, value);
+				rightHandPoser.PrimaryPose.Type = BlendType.BooleanParameter;
+			}
+			if (!isLeftPrimaryButtonPressed && !isRightPrimaryButtonPressed) {
 				float value = Mathf.Clamp(meshRenderer.GetBlendShapeWeight(0) - Time.deltaTime * speed * 100, 0, 100);
 				meshRenderer.SetBlendShapeWeight(0, value);
 			}
 		} else {
-			poser.PrimaryPose.Type = BlendType.Immediate;
+			meshRenderer.SetBlendShapeWeight(0, 0);
+			leftHandPoser.PrimaryPose.Type = BlendType.Immediate;
+			rightHandPoser.PrimaryPose.Type = BlendType.Immediate;
 		}
 	}
+
+	public void Grabbed() => grabCount++;
+
+	public void Released() => grabCount--;
+
 }
