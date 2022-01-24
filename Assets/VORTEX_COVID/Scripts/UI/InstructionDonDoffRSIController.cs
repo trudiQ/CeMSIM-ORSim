1697d5b5886393a38aab2c7ba4071f6ef854dee4
Methunas <b1756chris@aol.com>
2022-01-24 14:58:33 -0500

    Added the instruction popup to the RSI scenario

---
+++ Assets/VORTEX_COVID/Scripts/UI/InstructionDonDoffRSIController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionDonDoffRSIController : MonoBehaviour
+{
+    public InstructionUI[] instructionPanels;
+    public AvatarSwapper avatarSwapper;
+
+    private bool roleSelected;
+    private bool donningComplete;
+    private bool rsiComplete;
+    
+    void Start()
+    {
+        foreach (InstructionUI panel in instructionPanels)
+        {
+            panel.UpdateProcedure("RSI");
+        }
+    }
+
+    private void AllPanelsDisplayNext()
+    {
+        foreach (InstructionUI panel in instructionPanels)
+        {
+            panel.DisplayNextText();
+        }
+    }
+
+    public void RoleSelected(string role)
+    {
+        foreach (InstructionUI panel in instructionPanels)
+        {
+            panel.UpdateRole(role);
+        }
+
+        if (!roleSelected)
+        {
+            roleSelected = true;
+            AllPanelsDisplayNext();
+        }
+    }
+
+    public void RoleSelected(int roleIndex, int avatarIndex)
+    {
+        string role = avatarSwapper.avatarLists[roleIndex].name;
+
+        foreach (InstructionUI panel in instructionPanels)
+        {
+            panel.UpdateRole(role);
+        }
+
+        if (!roleSelected)
+        {
+            roleSelected = true;
+            AllPanelsDisplayNext();
+        }
+    }
+
+    public void DonningComplete()
+    {
+        if (!donningComplete)
+        {
+            donningComplete = true;
+            AllPanelsDisplayNext();
+        }
+    }
+    
+    public void RSIComplete()
+    {
+        if (!rsiComplete)
+        {
+            rsiComplete = true;
+            AllPanelsDisplayNext();
+        }
+    }
+}
