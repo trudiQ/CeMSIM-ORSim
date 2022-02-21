2130aa8390be37fda7d692ed031307f41131261d
Yasar <ykakdas@gmail.com>
2022-02-21 13:29:04 -0600

    Added jaw lifting animation.

--- Assets/ETCollisionDetection.cs
+++ Assets/ETCollisionDetection.cs
@@ -1,12 +1,12 @@
-﻿using UnityEngine;
+﻿using HurricaneVR.Framework.Core;
+using UnityEngine;
 
 public class ETCollisionDetection : MonoBehaviour {
 	public Collider etTriggerCollider;
 	public Collider cheekCollider;
-
 	public Transform anchor;
-
 	public TransformHolder jawETInsertionTransform;
+	public HVRGrabbable grabbable;
 
 	// Start is called before the first frame update
 	void Start() {
@@ -28,10 +28,16 @@ public class ETCollisionDetection : MonoBehaviour {
 		if (tubeInteraction) {
 			etTriggerCollider.enabled = false;
 			cheekCollider.enabled = false;
+			grabbable.enabled = false;
 			SetJawTransform();
 			GetComponentInChildren<TongueBladeInteractions>().isETInserted = true;
 			other.transform.parent = this.transform;
 			tubeInteraction.StartInsertionAnimation(anchor);
 		}
+	}
+
+	public void ReleaseJaw() {
+		grabbable.ForceRelease();
+		grabbable.CanBeGrabbed = false;
 	}
 }
--- Assets/Project/Scripts/RSI/TongueBladeInteractions.cs
+++ Assets/Project/Scripts/RSI/TongueBladeInteractions.cs
@@ -7,6 +7,7 @@ public class TongueBladeInteractions : MonoBehaviour {
 	public bool isETInserted = false;
 	public Transform bladeAnchor;
 	public ETCollisionDetection etCollisionDetection;
+	public Animator jawAnimator;
 
 	private bool staying = false;
 	private GameObject blade;
@@ -24,6 +25,7 @@ public class TongueBladeInteractions : MonoBehaviour {
 
 	public void FreezeBladeOnButtonPress() {
 		if (blade && !bladeLocked) {
+			etCollisionDetection.ReleaseJaw();
 			bladeLocked = true;
 			blade.transform.SetParent(bladeAnchor.parent, true);
 			StartCoroutine(LerpTransform(blade.transform, bladeAnchor));
@@ -40,12 +42,23 @@ public class TongueBladeInteractions : MonoBehaviour {
 	public IEnumerator LerpTransform(Transform from, Transform to) {
 		float timeElapsed = 0f;
 		while (timeElapsed <= 2f) {
-			from.localPosition = Vector3.Lerp(from.localPosition, to.localPosition, timeElapsed / 2);
-			from.localRotation = Quaternion.Lerp(from.localRotation, to.localRotation, timeElapsed / 2);
-			from.localScale = Vector3.Lerp(from.localScale, to.localScale, timeElapsed / 2);
+
+			if (Vector3.Distance(from.localPosition, to.localPosition) < 0.001) {
+				from.localPosition = to.localPosition;
+				from.localRotation = to.localRotation;
+				from.localScale = to.localScale;
+				jawAnimator.enabled = true;
+				yield break;
+			} else {
+				from.localPosition = Vector3.Lerp(from.localPosition, to.localPosition, timeElapsed / 2);
+				from.localRotation = Quaternion.Lerp(from.localRotation, to.localRotation, timeElapsed / 2);
+				from.localScale = Vector3.Lerp(from.localScale, to.localScale, timeElapsed / 2);
+			}
+
 			timeElapsed += Time.deltaTime;
 			yield return null;
 		}
+
 	}
 
 	private void OnTriggerEnter(Collider other) {
