4baa6424ce739a3c640d05a4226ccd5fff313ce2
Yasar <ykakdas@gmail.com>
2022-03-14 22:40:56 -0500

    Added securing et animations and tapes.

--- Assets/ConnectorSyringeAnimations.cs
+++ Assets/ConnectorSyringeAnimations.cs
@@ -3,56 +3,60 @@ using HurricaneVR.Framework.Core.Grabbers;
 using UnityEngine;
 
 public class ConnectorSyringeAnimations : MonoBehaviour {
-    public TubeInteraction tubeInteraction;
-    public Animator animator;
-    public Collider syringeCollider;
+	public TubeInteraction tubeInteraction;
+	public Animator animator;
+	public Collider syringeCollider;
+	public Animator tapingAnimator;
 
-    private HVRInteractable interactable;
-    private Rigidbody rb;
-    private bool isSyringeDetached = false;
-    private HVRHandGrabber grabber;
+	private HVRInteractable interactable;
+	private Rigidbody rb;
+	private bool isSyringeDetached = false;
+	private HVRHandGrabber grabber;
 
-    // Start is called before the first frame update
-    void Start() {
-        interactable = GetComponent<HVRInteractable>();
-        rb = GetComponent<Rigidbody>();
-        interactable.HandGrabbed.AddListener(OnGrabbed);
-        interactable.HandReleased.AddListener(OnReleased);
-    }
+	// Start is called before the first frame update
+	void Start() {
+		interactable = GetComponent<HVRInteractable>();
+		rb = GetComponent<Rigidbody>();
+		interactable.HandGrabbed.AddListener(OnGrabbed);
+		interactable.HandReleased.AddListener(OnReleased);
+	}
 
-    // Update is called once per frame
-    void Update() { }
+	// Update is called once per frame
+	void Update() { }
 
-    public void DetachSyringe() {
-        rb.isKinematic = false;
-        isSyringeDetached = true;
-        syringeCollider.isTrigger = false;
-        transform.SetParent(null);
-        interactable.ForceRelease();
-        grabber.TryGrab(interactable, true);
-        animator.Play("ETConnectorRestoreAnim");
-    }
+	public void DetachSyringe() {
+		rb.isKinematic = false;
+		isSyringeDetached = true;
+		syringeCollider.isTrigger = false;
+		transform.SetParent(null);
+		interactable.ForceRelease();
+		grabber.TryGrab(interactable, true);
+		animator.Play("ETConnectorRestoreAnim");
+		Invoke(nameof(SecureET), 2f);
+	}
 
-    private void OnGrabbed(HVRHandGrabber grabber, HVRGrabbable grabbable) {
-        this.grabber = grabber;
-        if (isSyringeDetached) return;
-        if (!tubeInteraction.isEtInserted) {
-            grabbable.ForceRelease();
-        }
-        else {
-            animator.enabled = true;
-            animator.Play("ETConnectorAnim");
-        }
-    }
+	private void SecureET() {
+		tapingAnimator.Play("SecureETAnim");
+	}
 
-    private void OnReleased(HVRHandGrabber grabber, HVRGrabbable grabbable) {
-        if (!isSyringeDetached) {
-            if (tubeInteraction.isEtInserted) {
-                animator.Play("ETConnectorRestoreAnim");
-            }
-        }
-        else {
-            rb.isKinematic = false;
-        }
-    }
+	private void OnGrabbed(HVRHandGrabber grabber, HVRGrabbable grabbable) {
+		this.grabber = grabber;
+		if (isSyringeDetached) return;
+		if (!tubeInteraction.isEtInserted) {
+			grabbable.ForceRelease();
+		} else {
+			animator.enabled = true;
+			animator.Play("ETConnectorAnim");
+		}
+	}
+
+	private void OnReleased(HVRHandGrabber grabber, HVRGrabbable grabbable) {
+		if (!isSyringeDetached) {
+			if (tubeInteraction.isEtInserted) {
+				animator.Play("ETConnectorRestoreAnim");
+			}
+		} else {
+			rb.isKinematic = false;
+		}
+	}
 }
