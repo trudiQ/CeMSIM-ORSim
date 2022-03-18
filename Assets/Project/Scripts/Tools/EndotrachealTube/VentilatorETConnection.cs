cc27ff3ce77e060c909e07dd8902769ffa5ab2e0
Yasar <ykakdas@gmail.com>
2022-03-18 03:29:16 -0500

    New model added for ventilator attachment.

--- Assets/Project/Scripts/Tools/EndotrachealTube/VentilatorETConnection.cs
+++ Assets/Project/Scripts/Tools/EndotrachealTube/VentilatorETConnection.cs
@@ -19,10 +19,17 @@ public class VentilatorETConnection : MonoBehaviour {
 			foreach (Collider c in GetComponents<Collider>()) {
 				c.enabled = false;
 			}
+
+			var parent = transform.parent;
+			transform.SetParent(other.gameObject.transform);
 			transform.localPosition = connectedTransform.position;
 			transform.localEulerAngles = connectedTransform.rotation;
+			transform.parent = parent;
+
 			interactable.ForceRelease();
 			interactable.grabbable = false;
+
+			GetComponent<Rigidbody>().isKinematic = true;
 		}
 	}
 }
