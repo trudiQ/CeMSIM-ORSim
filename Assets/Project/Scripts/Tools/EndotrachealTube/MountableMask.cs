06c06ada0b41fea743e75a5593960d6be5fc56f9
Yasar <ykakdas@gmail.com>
2022-03-18 19:35:43 -0500

    Added mounting mask logics to the ventilator connector.

---
+++ Assets/Project/Scripts/Tools/EndotrachealTube/MountableMask.cs
@@ -0,0 +1,63 @@
+﻿using HurricaneVR.Framework.Core;
+using HurricaneVR.Framework.Core.Grabbers;
+using UnityEngine;
+
+public class MountableMask : MonoBehaviour {
+	[HideInInspector]
+	public bool mounted;
+
+	public Collider mountableCollider;
+	public HVRInteractable grabbable;
+	public float breakForce = 150f;
+	public VentilatorETConnection manager;
+
+	private FixedJoint joint;
+	private bool isGrabbed;
+
+	// Start is called before the first frame update
+	void Start() {
+		grabbable.Grabbed.AddListener(OnGrabbed);
+		grabbable.Released.AddListener(OnReleased);
+		if (!manager) {
+			manager = FindObjectOfType<VentilatorETConnection>();
+		}
+	}
+
+	// Update is called once per frame
+	void Update() {
+		CheckBreakForce();
+		CheckMountState();
+	}
+
+	private void CheckBreakForce() {
+		if (joint && isGrabbed) {
+			Vector3 projection = Vector3.Project(joint.currentForce, transform.right);
+			if (projection.magnitude > breakForce) {
+				Destroy(joint);
+				Invoke(nameof(OnJointBreak), 1f);
+			}
+		}
+	}
+
+	void OnJointBreak() {
+		manager.isAttached = false;
+		grabbable.Stationary = false;
+		mountableCollider.enabled = true;
+	}
+
+	private void CheckMountState() {
+		if (mounted) {
+			mountableCollider.enabled = false;
+			joint = GetComponent<FixedJoint>();
+			mounted = false;
+		}
+	}
+
+	private void OnGrabbed(HVRGrabberBase grabber, HVRGrabbable grabbable) {
+		isGrabbed = true;
+	}
+
+	private void OnReleased(HVRGrabberBase grabber, HVRGrabbable grabbable) {
+		isGrabbed = false;
+	}
+}
--- Assets/Project/Scripts/Tools/EndotrachealTube/VentilatorETConnection.cs
+++ Assets/Project/Scripts/Tools/EndotrachealTube/VentilatorETConnection.cs
@@ -4,9 +4,13 @@ public class VentilatorETConnection : MonoBehaviour {
 	public TransformHolder connectedTransform;
 	public HVRInteractable interactable;
 
+	[HideInInspector]
+	public bool isAttached;
+
+	private Rigidbody rb;
 	// Start is called before the first frame update
 	void Start() {
-
+		rb = GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
@@ -15,21 +19,45 @@ public class VentilatorETConnection : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.GetComponent<TubeInteraction>()) {
-			foreach (Collider c in GetComponents<Collider>()) {
-				c.enabled = false;
-			}
+		MountableMask mountableMask = other.gameObject.GetComponent<MountableMask>();
+		TubeInteraction tubeInteraction = other.gameObject.GetComponent<TubeInteraction>();
 
-			var parent = transform.parent;
-			transform.SetParent(other.gameObject.transform);
-			transform.localPosition = connectedTransform.position;
-			transform.localEulerAngles = connectedTransform.rotation;
-			transform.parent = parent;
-
-			interactable.ForceRelease();
-			interactable.grabbable = false;
-
-			GetComponent<Rigidbody>().isKinematic = true;
+		if (tubeInteraction) {
+			AttachTube(tubeInteraction, other.gameObject.transform);
+		} else if (mountableMask && !isAttached) {
+			AttachMask(mountableMask, other.gameObject);
 		}
+	}
+
+	private void AttachMask(MountableMask mountableMask, GameObject otherGameObject) {
+		TransformHolder transformHolder = otherGameObject.GetComponent<TransformHolder>();
+		Transform otherTransform = otherGameObject.transform;
+		otherTransform.parent = transform;
+		otherTransform.localPosition = transformHolder.position;
+		otherTransform.localEulerAngles = transformHolder.rotation;
+		otherTransform.SetParent(null, true);
+
+		FixedJoint joint = otherGameObject.AddComponent<FixedJoint>();
+		joint.connectedBody = rb;
+		mountableMask.mounted = true;
+		mountableMask.grabbable.Stationary = true;
+		isAttached = true;
+	}
+
+	private void AttachTube(TubeInteraction tubeInteraction, Transform transform) {
+		foreach (Collider c in GetComponents<Collider>()) {
+			c.enabled = false;
+		}
+
+		var parent = transform.parent;
+		transform.SetParent(transform);
+		transform.localPosition = connectedTransform.position;
+		transform.localEulerAngles = connectedTransform.rotation;
+		transform.parent = parent;
+
+		interactable.ForceRelease();
+		interactable.grabbable = false;
+
+		rb.isKinematic = true;
 	}
 }
